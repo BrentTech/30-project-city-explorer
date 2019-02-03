899338444a2f91fbc1b73d275e829057c6bf7765
Brent Woodward <Brent.Woodward.1@gmail.com>
2019-02-03 10:19:19 -0800

    deployment and README

---
+++ city-explorer/build/precache-manifest.71f1cb1e3492c99a71e3154b47e8114f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40ece35da72a171cf451",
+    "url": "/static/css/main.30fde898.chunk.css"
+  },
+  {
+    "revision": "40ece35da72a171cf451",
+    "url": "/static/js/main.40ece35d.chunk.js"
+  },
+  {
+    "revision": "e508ac3db384a8c968ed",
+    "url": "/static/js/1.e508ac3d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82ab95b0fafb855b8bc6243fe069b321",
+    "url": "/index.html"
+  }
+];
